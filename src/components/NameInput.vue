748a5f858aca348797b1d5d7c93aa74133c9b04e
KumaBar <albermangutov@yandex.ru>
2021-09-17 08:28:55 +0000

    clean

--- src/components/NameInput.vue
+++ src/components/NameInput.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="form-input">
     <div class="form-input-title">Имя</div>
-    <div v-if="nameError" class='form-input-error'>Заполните правильно поле</div>
-    <div  class="form-input-text">
+    <div v-if="nameError" class="form-input-error">Заполните правильно поле</div>
+    <div class="form-input-text">
       <input v-model="name" placeholder="Введите Ваше имя" />
     </div>
   </div>
@@ -18,17 +18,22 @@ export default {
     };
   },
   computed: {
-      nameError(){
-          return !this.form.nameConfirmed
-      }
+    nameError() {
+      return !this.form.nameConfirmed;
+    },
   },
-  methods: {},
+  methods: {
+    setNameError(value) {
+      this.form.confirmed = value;
+    },
+  },
   watch: {
     name() {
       const regex = /^[a-zа-яё\s]+$/iu; //eslint-disable-line
       if (regex.test(this.name)) {
         console.log("только буквы");
         this.form.nameConfirmed = true;
+        this.setNameError(true);
       } else {
         console.log("не только буквы");
         this.form.nameConfirmed = false;
